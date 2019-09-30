f8f05fd1dd225b9fc5f748f0ff09c311243bfc5d
Gabranth <charlesbourtoire@gmail.com>
2019-09-30 10:38:27 +0200

    Commencement architecture simulation

--- 1-modules/calcul_obtention/index.php
+++ 1-modules/calcul_obtention/index.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html lang="en">
   
-  <?php Head("Simulation obtention BTS SIO", 2, 0); ?>
+  <?php Head("Simulation obtention BTS SIO", 8, 0); ?>
   <section class="container">
   	<div class="row">
   		<h1>Accueil</h1>
@@ -14,40 +14,60 @@
 
   	<br>
 
-  	<div class="row">
-  		<div class="col">
-  			<div class="card" style="width: 18rem;">
-				  <div class="card-body">
-				    <h5 class="card-title">Méthodologie</h5>
-				    <p><small>Mme Charabidze</small></p>
-				    <hr>
-				    <a href="#" class="btn btn-primary">Aller</a>
-				  </div>
-				</div>
-			</div>
+		<form action="index.php" method="POST" class="form-group">
+			<!-- Coefficient des matières de BTS en examen : 
+				- Culture G : 2;
+				- Mathématiques : 2;
+				- Anglais : 2;
+				- Algorithmique appliquée : 1;
+				- Economie : 1;
+				- Droit : 1;
+				- Management : 1;
+				- E4 : 4;
+				- E5 : 5;
+				- E6 : 3;
+				---- OPTIONS FACULTATIVES ----
+				- LV2 : 1;
+				- Maths appro : 1;
+			-->
+	  	<div class="row">
+	  		<div class="col-md-6">
+	  			<div class="row mb-2">
+							<div class="col-md-8">
+								<label for="noteCultureG">Culture Générale <small>(coefficient <strong>2</strong>)</small> : </label>
+							</div>
+							<div class="col-md-4">
+								<input id="noteCultureG" name="noteCultureG" type="number" class="form-control">
+							</div>
+	  			</div>
 
-			<div class="col">
-  			<div class="card" style="width: 18rem;">
-				  <div class="card-body">
-				    <h5 class="card-title">Seuls avec tous</h5>
-				    <p><small>Mme Charabidze</small></p>
-				    <hr>
-				    <a href="#" class="btn btn-primary">Aller</a>
-				  </div>
-				</div>
-			</div>
+	  			<div class="row mb-2">
+							<div class="col-md-8">
+								<label for="noteMaths">Mathématiques <small>(coefficient <strong>2</strong>)</small> : </label>
+							</div>
+							<div class="col-md-4">
+								<input id="noteMaths" name="noteMaths" type="number" class="form-control">
+							</div>
+	  			</div>
 
-			<div class="col">
-  			<div class="card" style="width: 18rem;">
-				  <div class="card-body">
-				    <h5 class="card-title">A toute vitesse</h5>
-				    <p><small>Mme Charabidze</small></p>
-				    <hr>
-				    <a href="#" class="btn btn-primary">Aller</a>
-				  </div>
+	  			<div class="row mb-2">
+							<div class="col-md-8">
+								<label for="noteAnglais">Anglais <small>(coefficient <strong>2</strong>)</small> : </label>
+							</div>
+							<div class="col-md-4">
+								<input id="noteAnglais" name="noteAnglais" type="number" class="form-control">
+							</div>
+	  			</div>
+
+				</div>
+
+				<div class="col-md-6 <?php (!isset($_POST["submit"]) || empty($_POST["submit"])) ? 'alert alert-info' : 'bloc' ?>">
+					<?php if (!isset($_POST["submit"]) || empty($_POST["submit"])) { ?>
+						<h2 class="alert alert-info text-center">Veuillez saisir vos notes.</h2>
+					<?php } ?>
 				</div>
 			</div>
-		</div>
+		</form>
 
   </section>
   <?php Js(); ?>
