ec5af4e641fcdf07bd7115d295a31a8508337dca
Gabranth <charlesbourtoire@gmail.com>
2019-10-05 22:04:31 +0200

    Module simulation convenable

--- 1-modules/calcul_obtention/app.vue.js
+++ 1-modules/calcul_obtention/app.vue.js
@@ -13,6 +13,7 @@ var app = new Vue({
 		math_appro:"",
 		langue_vivante:"",
 		moyenne:0,
+		autorise:0,
 	},
 	methods:{
 		CalculerMoyenne:function(){
@@ -24,66 +25,89 @@ var app = new Vue({
 				
 			}
 		},
+		Autorisation:function(){
+			var autorise = (this.culture_generale != "" && this.maths != "" && this.algorithmie != "" && this.anglais != "" && this.edm != "" && this.e4 != "" && this.e5 != "" && this.e6 != "") ? true : false;
+			if(autorise == true){
+				if(this.matieres_option == 1){
+					if(this.math_appro != "" || this.langue_vivante != ""){
+						this.autorise = 1;
+					}
+					else this.autorise = 0;
+				}
+				else this.autorise = 1;
+			}
+			else this.autorise = 0;
+		},
 	},
 	watch:{
 		culture_generale(){
 			if (this.culture_generale != "")
 			{
 				if (this.culture_generale > 20) this.culture_generale = 20;
+				this.Autorisation();
 			}
 		},
 		maths(){
 			if (this.maths != "")
 			{
 				if (this.maths > 20) this.maths = 20;
+				this.Autorisation();
 			}
 		},
 		algorithmie(){
 			if (this.algorithmie != "")
 			{
 				if (this.algorithmie > 20) this.algorithmie = 20;
+				this.Autorisation();
 			}
 		},
 		anglais(){
 			if (this.anglais != "")
 			{
 				if (this.anglais > 20) this.anglais = 20;
+				this.Autorisation();
 			}
 		},
 		edm(){
 			if (this.edm != "")
 			{
 				if (this.edm > 20) this.edm = 20;
+				this.Autorisation();
 			}
 		},
 		e4(){
 			if (this.e4 != "")
 			{
 				if (this.e4 > 20) this.e4 = 20;
+				this.Autorisation();
 			}
 		},
 		e5(){
 			if (this.e5 != "")
 			{
 				if (this.e5 > 20) this.e5 = 20;
+				this.Autorisation();
 			}
 		},
 		e6(){
 			if (this.e6 != "")
 			{
 				if (this.e6 > 20) this.e6 = 20;
+				this.Autorisation();
 			}
 		},
 		math_appro(){
 			if (this.math_appro != "")
 			{
 				if (this.math_appro > 20) this.math_appro = 20;
+				this.Autorisation();
 			}
 		},
 		langue_vivante(){
 			if (this.langue_vivante != "")
 			{
 				if (this.langue_vivante > 20) this.langue_vivante = 20;
+				this.Autorisation();
 			}
 		},
 		matieres_option(){
@@ -91,6 +115,7 @@ var app = new Vue({
 				this.math_appro = "";
 				this.langue_vivante = "";
 			}
+			else this.Autorisation();
 		},
 	},
 })
--- 1-modules/calcul_obtention/index.php
+++ 1-modules/calcul_obtention/index.php
@@ -3,7 +3,7 @@
 <html lang="en">
   
   <?php Head("Simulation obtention BTS SIO", 8, 0); ?>
-  <main id="app">
+  <main id="app" v-cloak>
 	  <section class="container">
 	  	<div class="row">
 	  		<h1>Accueil</h1>
@@ -36,7 +36,7 @@
 									<label for="noteCultureG">Culture Générale <small>(coefficient <strong>2</strong>)</small> : </label>
 								</div>
 								<div class="col-md-3">
-									<input id="noteCultureG" name="noteCultureG" type="number" min="0" max="20" class="form-control" v-model="culture_generale">
+									<input id="noteCultureG" name="noteCultureG" type="number" required min="0" max="20" class="form-control" v-model="culture_generale">
 								</div>
 								<div class="col-md-1 mt-2">
 									<span v-if="culture_generale == ''"  class="text-danger"><i class="fas fa-ban"></i></span>
@@ -51,7 +51,7 @@
 									<label for="noteAnglais">Anglais <small>(coefficient <strong>2</strong>)</small> : </label>
 								</div>
 								<div class="col-md-3">
-									<input id="noteAnglais" name="noteAnglais" type="number" min="0" max="20" class="form-control" v-model="anglais">
+									<input id="noteAnglais" name="noteAnglais" type="number" required min="0" max="20" class="form-control" v-model="anglais">
 								</div>
 								<div class="col-md-1 mt-2">
 									<span v-if="anglais == ''"  class="text-danger"><i class="fas fa-ban"></i></span>
@@ -66,7 +66,7 @@
 									<label for="noteMaths">Mathématiques <small>(coefficient <strong>2</strong>)</small> : </label>
 								</div>
 								<div class="col-md-3">
-									<input id="noteMaths" name="noteMaths" type="number" min="0" max="20" class="form-control" v-model="maths">
+									<input id="noteMaths" name="noteMaths" type="number" required min="0" max="20" class="form-control" v-model="maths">
 								</div>
 								<div class="col-md-1 mt-2">
 									<span v-if="maths == ''"  class="text-danger"><i class="fas fa-ban"></i></span>
@@ -81,7 +81,7 @@
 									<label for="noteAlgorithmie">Algorithmie <small>(coefficient <strong>1</strong>)</small> : </label>
 								</div>
 								<div class="col-md-3">
-									<input id="noteAlgorithmie" name="noteAlgorithmie" type="number" min="0" max="20" class="form-control" v-model="algorithmie">
+									<input id="noteAlgorithmie" name="noteAlgorithmie" type="number" required min="0" max="20" class="form-control" v-model="algorithmie">
 								</div>
 								<div class="col-md-1 mt-2">
 									<span v-if="algorithmie == ''"  class="text-danger"><i class="fas fa-ban"></i></span>
@@ -96,7 +96,7 @@
 									<label for="noteEDM">Economie/Droit/Management <small>(coefficient <strong>3</strong>)</small> : </label>
 								</div>
 								<div class="col-md-3">
-									<input id="noteEDM" name="noteEDM" type="number" min="0" max="20" class="form-control" v-model="edm">
+									<input id="noteEDM" name="noteEDM" type="number" required min="0" max="20" class="form-control" v-model="edm">
 								</div>
 								<div class="col-md-1 mt-2">
 									<span v-if="edm == ''"  class="text-danger"><i class="fas fa-ban"></i></span>
@@ -111,7 +111,7 @@
 									<label for="noteE4">Conception et maintenance de solutions informatiques <small>(coefficient <strong>4</strong>)</small> : </label>
 								</div>
 								<div class="col-md-3">
-									<input id="noteE4" name="noteE4" type="number" min="0" max="20" class="form-control" v-model="e4">
+									<input id="noteE4" name="noteE4" type="number" required min="0" max="20" class="form-control" v-model="e4">
 								</div>
 								<div class="col-md-1 mt-2">
 									<span v-if="e4 == ''"  class="text-danger"><i class="fas fa-ban"></i></span>
@@ -126,7 +126,7 @@
 									<label for="noteE5">Production et fourniture de services informatiques <small>(coefficient <strong>5</strong>)</small> : </label>
 								</div>
 								<div class="col-md-3">
-									<input id="noteE5" name="noteE5" type="number" min="0" max="20" class="form-control" v-model="e5">
+									<input id="noteE5" name="noteE5" type="number" required min="0" max="20" class="form-control" v-model="e5">
 								</div>
 								<div class="col-md-1 mt-2">
 									<span v-if="e5 == ''"  class="text-danger"><i class="fas fa-ban"></i></span>
@@ -141,7 +141,7 @@
 									<label for="noteE6">Parcours de professionnalisation <small>(coefficient <strong>3</strong>)</small> : </label>
 								</div>
 								<div class="col-md-3">
-									<input id="noteE6" name="noteE6" type="number" min="0" max="20" class="form-control" v-model="e6">
+									<input id="noteE6" name="noteE6" type="number" required min="0" max="20" class="form-control" v-model="e6">
 								</div>
 								<div class="col-md-1 mt-2">
 									<span v-if="e6 == ''"  class="text-danger"><i class="fas fa-ban"></i></span>
@@ -163,7 +163,7 @@
 									<label for="noteMathsAppro">Mathématiques approfondies <small>(coefficient <strong>1</strong>)</small> : </label>
 								</div>
 								<div class="col-md-3">
-									<input id="noteMathsAppro" name="noteMathsAppro" type="number" min="0" max="20" class="form-control" v-model="math_appro">
+									<input id="noteMathsAppro" name="noteMathsAppro" type="number" :required="matieres_option == 1" min="0" max="20" class="form-control" v-model="math_appro">
 								</div>
 								<div class="col-md-1 mt-2">
 									<span v-if="math_appro == ''"  class="text-danger"><i class="fas fa-ban"></i></span>
@@ -178,7 +178,7 @@
 									<label for="noteLangueVivante">Langue secondaire <small>(coefficient <strong>1</strong>)</small> : </label>
 								</div>
 								<div class="col-md-3">
-									<input id="noteLangueVivante" name="noteLangueVivante" type="number" min="0" max="20" class="form-control" v-model="langue_vivante">
+									<input id="noteLangueVivante" name="noteLangueVivante" type="number" :required="matieres_option == 1" min="0" max="20" class="form-control" v-model="langue_vivante">
 								</div>
 								<div class="col-md-1 mt-2">
 									<span v-if="langue_vivante == ''"  class="text-danger"><i class="fas fa-ban"></i></span>
@@ -188,14 +188,51 @@
 								</div>
 		  				</div>
 		  			</div>
+						
+						<div class="text-right">
+		  				<input v-show="autorise == 1" type="submit" class="btn btn-sm btn-outline-primary" value="Valider" name="submit" id="submit">
+		  				<a href="#" v-show="autorise == 0" class="btn btn-sm btn-outline-dark" style="cursor:not-allowed;">Valider</a>
+						</div>
 
 					</div>
 
-					<div class="col-md-6 <?php (!isset($_POST["submit"]) || empty($_POST["submit"])) ? 'alert alert-info' : 'bloc' ?>">
+					<div class="col-md-6">
 						<?php if (!isset($_POST["submit"]) || empty($_POST["submit"])) { ?>
 							<h2 class="alert alert-info text-center">Veuillez saisir vos notes.</h2>
-						<?php } else { ?>
-							
+						<?php } else { 
+							$noteCultureG = (isset($_POST["noteCultureG"]) && (!empty($_POST["noteCultureG"]) && $_POST["noteCultureG"] != 0)) ? $_POST["noteCultureG"] : "";
+							$noteAnglais = (isset($_POST["noteAnglais"]) && (!empty($_POST["noteAnglais"]) && $_POST["noteAnglais"] != 0)) ? $_POST["noteAnglais"] : "";
+							$noteMaths = (isset($_POST["noteMaths"]) && (!empty($_POST["noteMaths"]) && $_POST["noteMaths"] != 0)) ? $_POST["noteMaths"] : "";
+							$noteAlgorithmie = (isset($_POST["noteAlgorithmie"]) && (!empty($_POST["noteAlgorithmie"]) && $_POST["noteAlgorithmie"] != 0)) ? $_POST["noteAlgorithmie"] : "";
+							$noteEDM = (isset($_POST["noteEDM"]) && (!empty($_POST["noteEDM"]) && $_POST["noteEDM"] != 0)) ? $_POST["noteEDM"] : "";
+							$noteE4 = (isset($_POST["noteE4"]) && (!empty($_POST["noteE4"]) && $_POST["noteE4"] != 0)) ? $_POST["noteE4"] : "";
+							$noteE5 = (isset($_POST["noteE5"]) && (!empty($_POST["noteE5"]) && $_POST["noteE5"] != 0)) ? $_POST["noteE5"] : "";
+							$noteE6 = (isset($_POST["noteE6"]) && (!empty($_POST["noteE6"]) && $_POST["noteE6"] != 0)) ? $_POST["noteE6"] : "";
+							$noteMathsAppro = (isset($_POST["noteMathsAppro"]) && (!empty($_POST["noteMathsAppro"]) && $_POST["noteMathsAppro"] != 0)) ? $_POST["noteMathsAppro"] : "";
+							$noteLangueVivante = (isset($_POST["noteLangueVivante"]) && (!empty($_POST["noteLangueVivante"]) && $_POST["noteLangueVivante"] != 0)) ? $_POST["noteLangueVivante"] : "";
+
+							$noteTotale = $noteCultureG*2+$noteAnglais*2+$noteMaths*2+$noteAlgorithmie+$noteEDM*3+$noteE4*4+$noteE5*5+$noteE6*3;
+							$nbNote = 22;
+							if($noteMathsAppro != ""){
+							 $noteTotale += ($noteMathsAppro > 10) ? $noteMathsAppro-10 : 0;
+							}
+							if($noteLangueVivante != ""){
+							 $noteTotale += ($noteLangueVivante > 10) ? $noteLangueVivante-10 : 0;
+							}
+							$nbPoints = round($noteTotale/$nbNote, 2);
+						?>
+							<div class="alert <?php echo ($noteTotale > 219) ? 'alert-success ' : 'alert-danger ' ?> text-center">
+								<h2 class="mt-2"><?php echo ($noteTotale > 219) ? "Beau travail, tu as le BTS !" : "Pas de chance, tu l'auras la prochaine fois !"; ?></h2>
+								<p>
+									Vous avez <?php echo $nbPoints ?>/20. <br> Ce qui signifie que vous 
+									<?php 
+										if($nbPoints < 12) echo "n'avez certainement pas de mention. :/";
+										if($nbPoints >= 12 && $nbPoints < 14) echo "avez certainement la mention assez bien !";
+										if($nbPoints >= 14 && $nbPoints < 16) echo "avez certainement la mention bien !";
+										if($nbPoints >= 16) echo "avez certainement la mention très bien !!!!!!!";
+									?>
+								</p>
+							</div>
 						<?php } ?>
 					</div>
 				</div>
@@ -205,3 +242,11 @@
   </main>
   <?php Js(); ?>
   <script src="app.vue.js"></script>
+  <script>
+  	$(document).keypress(
+  		function(event){
+  			if(event.which == '13'){
+  				event.preventDefault();
+  			}
+		});
+  </script>
